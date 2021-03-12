3399cf555964121c8fcf3ea2e79e3cd0578635ca
Borys <bogori92@gmail.com>
2021-03-12 19:19:35 +0100

    ready to use

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelector('.show-modal');
+
+btnOpenModal.addEventListener('click', showModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+function showModal() {
+  overlay.classList.remove('hidden');
+  modal.classList.remove('hidden');
+}
+
+function closeModal() {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+}
+
+window.addEventListener('keydown', e => {
+  if (e.key === 'Escape') closeModal();
+});
